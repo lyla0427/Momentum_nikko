2d647254596a2528bb69f308c7a4f4f47249de55
lyla0427 <hillary0427@gmail.com>
2022-04-14 22:39:15 +0900

    use handlers!

--- app.js
+++ app.js
@@ -1,1 +1,35 @@
-alert("hi");
+const title = document.querySelector("div.hello:first-child h1");
+console.log(title)
+title.innerText = "Hello"
+title.style.color = "blue";
+function handleTitleClick(){
+    title.style.color = "pink";
+    title.innerText = "You clicked"
+
+}
+
+function handleMouseEnter(){
+    title.style.color = "black";
+    title.innerText = "You hovered"
+}
+
+function handleMouseLeave(){
+    title.style.color = "White";
+    title.innerText = "You left"
+}
+
+function handleWindowResize(){
+    document.body.style.backgroundColor = "tomato"
+    title.innerText = "You resized"
+}
+
+function handleRightMouseEnter(){
+    document.body.style.backgroundColor = "blue"
+    title.innerText = "You right clicked"
+}
+
+title.addEventListener("click", handleTitleClick)
+title.addEventListener("mouseenter", handleMouseEnter)
+title.addEventListener("mouseleave", handleMouseLeave)
+window.addEventListener("contextmenu", handleRightMouseEnter)
+window.addEventListener("resize", handleWindowResize)
